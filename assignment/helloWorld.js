a63bc3048c60b7fb42c49102bae7bbdb32b275eb
Ying Her <yher.prime@gmail.com>
2023-11-05 17:54:28 -0600

    html & css completed

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -1,1 +1,22 @@
-console.log("Hello World")
+// Code Comment Hi There
+console.log('Hello World')
+
+const firstName = 'Ying'
+const lastName = 'Her'
+console.log(firstName,"",lastName)
+
+const cat = 1;
+const dog = 10
+const bird = 15
+
+if (bird > cat && bird < dog) {
+    console.log('Value is between');
+} else {
+    console.log('Not between')
+}
+
+const falsy = undefined;
+
+if (falsy == null) {
+    console.log('null or undefined');
+}
